e099665e19c1d047aeb3f257d2118a1282b2ff65
FellipeLorram <fellipelorram@gmail.com>
2025-03-29 22:46:06 -0300

    Add SignInForm, Avatar, and Dialog components: Implement user authentication form, avatar UI, and dialog functionality for enhanced user interaction and interface consistency.

---
+++ src/components/forms/sign-in-form.tsx
@@ -0,0 +1,66 @@
+import { useForm } from "react-hook-form";
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { z } from "zod";
+import { useMutation } from "@tanstack/react-query";
+import { signIn } from "@/api/auth/sign-in";
+import { Loader2 } from "lucide-react";
+
+const formSchema = z.object({
+  email: z.string().email(),
+});
+
+interface Props {
+  onSuccess?: () => void;
+}
+
+export function SignInForm({ onSuccess }: Props) {
+  const { mutate, isPending } = useMutation({
+    mutationFn: signIn,
+    onSuccess,
+  });
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+    },
+  });
+
+  function onSubmit(data: z.infer<typeof formSchema>) {
+    mutate(data);
+  }
+
+  return (
+    <Form {...form}>
+      <form className="space-y-4" onSubmit={form.handleSubmit(onSubmit)}>
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input type="email" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit" className="w-full" disabled={isPending}>
+          {isPending && <Loader2 className="w-4 h-4 animate-spin" />}
+          Entrar
+        </Button>
+      </form>
+    </Form>
+  );
+}
